3c79b2146361a0c983abb90c6c8f8cb769472913
github username <github email address>
2019-11-08 14:59:26 -0500

    Peter

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,4 +3,17 @@ class Store < ApplicationRecord
     has_many :drinks, through: :store_drink
     validates :name, presence: true
     validates :location, presence: true, uniqueness: true
+
+    def menu
+        storedrinks.map do |sd|
+          sd.drink
+        end
+      end
+    
+      def storedrinks
+        StoreDrink.all.select do |sd|
+          sd.store_id == self.id
+        end
+      end
+      
 end
